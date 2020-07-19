29cde57c16c686db36c506eaa509721b1c368fb7
sharadcodes <36638057+sharadcodes@users.noreply.github.com>
2020-07-19 13:52:40 +0530

    Fix bug and rel 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='vidlist',
-    version='0.0.2',
+    version='0.0.3',
     author="Sharad Raj Singh Maurya",
     author_email="iamsharadrah@gmail.com",
     url="https://github.com/sharadcodes/vidlist",
